00961580ebc438b3aa7f7d50effeabdf32ea4e2f
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-09-18 18:40:02 -0400

    return

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ Use our custom built Machine Learning Models to ease the educational process and
 if st.button('Return to Dashboard'):
     webbrowser.open('https://diplomatica.vercel.app/dashboard', new=0)
 
-# st.markdown('<button style="border-color: black; border-radius: 10;" ><a style="-webkit-appearance: button; -moz-appearance: button; appearance: button; text-decoration: none; color: initial;" href="https://diplomatica.vercel.app/dashboard">Return to Dashboard</a></button>', unsafe_allow_html=True)
+st.markdown('<button style="border-color: black; border-radius: 10;" ><a style="-webkit-appearance: button; -moz-appearance: button; appearance: button; text-decoration: none; color: initial;" href="https://diplomatica.vercel.app/dashboard" target="_blank">Return to Dashboard</a></button>', unsafe_allow_html=True)
 
-# st.markdown('[Return to Dashboard](https://diplomatica.vercel.app/dashboard)', unsafe_allow_html=True)
+st.markdown('[Return to Dashboard](https://diplomatica.vercel.app/dashboard)', unsafe_allow_html=True)
 
 # Other pages
 app.add_app("NLP Article Summary", summarize.app)
